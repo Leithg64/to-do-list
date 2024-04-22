42a3e90fc35e63b1880787a20cb7770b1ac8f778
Leith Green <leithgwrk22@gmail.com>
2024-04-22 08:50:18 +0100

    Changed pathing and some semantics code
    should fix newItem function

--- js/script.js
+++ js/script.js
@@ -1,37 +1,32 @@
-function newItem() {
-    //1. Adding a new item to the list of items:
-    let li = $("<li></li>");
-    let inputValue = $("#input").val();
-    let span = $("<span></span>").append(inputValue); 
-    li.append(span);
-  
-    if ($("#input").val() === "") {
+$(document).ready(function() {
+  function newItem() {
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.text(inputValue);
+
+    if (inputValue === '') {
       alert("You must write something!");
     } else {
-      let list = $("#list");
-      list.append(li);
-      $('#input').val(''); //clears the input after adding the item
+      $('#list').append(li);
     }
 
-    //2. Crossing out an item from the list of items:
-    li.on("dblclick", function () {
-      span.toggleClass("strike");
-      li.toggleClass("opacity");
+    li.on("dblclick", function() {
+      li.toggleClass("strike");
     });
-  
-    //3(i). Adding the delete button "X":
-    let crossOutButton = $("<crossOutButton></crossOutButton>");
-    $(crossOutButton).append("X");
+
+    let crossOutButton = $('<button></button>');
+    crossOutButton.text('X');
+    crossOutButton.addClass("crossOutButton"); // Add class to the button
     li.append(crossOutButton);
-  
-    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-  
-    crossOutButton.on("click", function () {
+
+    crossOutButton.on("click", function() {
       li.addClass("delete");
     });
-  
-    // 4. Reordering the items:
-    $("#list").sortable();
-}
+
+    $('#list').sortable();
+  }
+
+  $('#button').on('click', newItem);
+});
 
 
